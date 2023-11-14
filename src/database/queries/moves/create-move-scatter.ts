831de61b9bb5d6152a90e298e92d5a03185478bc
Nick-M1 <nicholasmalloura@gmail.com>
2023-11-14 19:47:15 +0000

    Initial Commit

--- src/database/queries/moves/create-move-scatter.ts
+++ src/database/queries/moves/create-move-scatter.ts
@@ -4,12 +4,12 @@ import {createIncrementingArray} from "../../../utils/array-utils.ts";
 import {randomFromOne} from "../../../utils/number-utils.ts";
 
 export default async function createMoveScatter(gameSessionId: string, playerId: string, xCoordinate: number, yCoordinate: number, thisPlayerMoves: Database['battleships']['Tables']['moves']['Row'][]) {
-    const moves = createIncrementingArray(1, 5)
-        .map(() => ({ x: randomFromOne(10), y: randomFromOne(10) }))
+    const movesTmp = createIncrementingArray(1, 5).map(() => ({ x: randomFromOne(10), y: randomFromOne(10) }))
+    movesTmp.push({ x: xCoordinate, y: yCoordinate })
+
+    const moves = movesTmp
         .filter(({ x, y }) => !thisPlayerMoves.some(move => move.x_coordinate === x && move.y_coordinate === y))
         .filter((item, index, self) => self.indexOf(item) == index)
-
-    moves.push({ x: xCoordinate, y: yCoordinate })
 
     const { data, error } = await supabase.from('moves')
         .insert(moves.map(move => ({ session_id: gameSessionId, player_id: playerId, x_coordinate: move.x, y_coordinate: move.y, move_type: 'scatter' } as const)))
